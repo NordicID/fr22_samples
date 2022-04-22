e8dc81ecc1c2ae7234759e555ce8afb7026b7b49
Jonas Munsin <jonas.munsin@nordicid.com>
2022-04-22 10:18:41 +0300

    SMAR-508: Add RPCDemo sample and documentation

---
+++ 2. RPCDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NidRpc;
+
+namespace RPCDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Client rpc = new Client("RPCDemo");
+            await rpc.ConnectAsync();
+            JObject rsp = await rpc.Call("api/builtin/sysinfo/versions");
+            if (rsp.ContainsKey("error"))
+            {
+                Console.WriteLine($"Error: {rsp["error"]}");
+            }
+            else
+            {
+                if (rsp.ContainsKey("version"))
+                {
+                    Console.WriteLine($"Firmware version is {rsp["version"]}");
+                }
+                else
+                {
+                    Console.WriteLine("Firmware version is missing!");
+                }
+            }
+        }
+    }
+}
